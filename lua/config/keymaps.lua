4565f047cc61fe24b6b7a282e9c656e2ba9c5a5e
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2025-04-28 12:31:15 +0530

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,59 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save file and quit
+keymap.set("n", "<Leader>w", ":update<Return>", opts)
+keymap.set("n", "<Leader>q", ":quit<Return>", opts)
+keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+
+-- File explorer with NvimTree
+-- keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
+keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
+
+-- Tabs
+keymap.set("n", "te", ":tabedit<Return>")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "tw", ":tabclose<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-S-h>", "<C-w><")
+keymap.set("n", "<C-S-l>", "<C-w>>")
+keymap.set("n", "<C-S-k>", "<C-w>+")
+keymap.set("n", "<C-S-j>", "<C-w>-")
+
+-- Diagnostics
+-- keymap.set("n", "<C-j>", function()
+-- 	vim.diagnostic.goto_next()
+-- end, opts)
+
+keymap.set("i", "jk", "<esc>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- replace the word you're on over the whole buffer
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gl<Left><Left><Left>]])
+
+-- move code blocks inside code blocks
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
