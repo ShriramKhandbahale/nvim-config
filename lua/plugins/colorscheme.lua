2235207eccd1ce63408403439ec4a71974074cf8
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2025-07-11 18:46:45 +0530

    change colorscheme to sonokai

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,12 @@
--- return {
--- 	{
--- 		"sainnhe/sonokai",
--- 		priority = 1000,
--- 		config = function()
--- 			vim.g.sonokai_transparent_background = "1"
--- 			vim.g.sonokai_enable_italic = "1"
--- 			vim.g.sonokai_style = "andromeda"
--- 			vim.cmd.colorscheme("sonokai")
--- 		end,
--- 	},
--- }
-
 return {
-	"AlphaTechnolog/pywal.nvim",
-	lazy = false,
-	name = "pywal",
-	config = function()
-		require("pywal").setup()
-
-		-- Set transparent background for common highlight groups
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-		vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-	end,
+	{
+		"sainnhe/sonokai",
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_transparent_background = "1"
+			vim.g.sonokai_enable_italic = "1"
+			vim.g.sonokai_style = "andromeda"
+			vim.cmd.colorscheme("sonokai")
+		end,
+	},
 }
